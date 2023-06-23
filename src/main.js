55bccc7ab6a8070f943cde023cf59f537b0a707f
harundogan01 <harun.dogan1n@gmail.com>
2023-06-23 22:31:15 +0300

    commit

--- src/main.js
+++ src/main.js
@@ -3,7 +3,5 @@ import App from './App.vue'
 import store from './store'
 import './assets/tailwind.css'
 import VueApexCharts from "vue3-apexcharts";
-import 'vuetify/styles'
-import Vuetify from 'vuetify'
 
-createApp(App).use(store).use(VueApexCharts).use(Vuetify).mount('#app')
+createApp(App).use(store).use(VueApexCharts).mount('#app')
